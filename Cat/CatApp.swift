22260a3f71a30069f8ecafaedc85b5272e41f3f0
Hallucinette <ameliepocchiolo42M@gmail.com>
2022-11-30 11:18:44 +0100

    First commit, creation Model

---
+++ Cat/CatApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatApp.swift
+//  Cat
+//
+//  Created by Amelie Pocchiolo on 30/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct CatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
