2334f02621d5a2b659de28cf6660219db0ebb3c7
Hallucinette <ameliepocchiolo42M@gmail.com>
2022-12-02 12:04:43 +0100

    View wainting finish.

---
+++ Cat/View/Helpers/CustList.swift
@@ -0,0 +1,40 @@
+//
+//  CustList.swift
+//  Cat
+//
+//  Created by Amelie Pocchiolo on 01/12/2022.
+//
+
+import SwiftUI
+
+struct CustList: View {
+    var pseudo: String
+    
+    var body: some View {
+        
+        VStack{
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(.gray, lineWidth: 2)
+                .frame(width: 300, height: 60)
+                .overlay {
+                    HStack(){
+                        VStack{
+                            Circle()
+                                .stroke(.pink, lineWidth: 2)
+                                .frame(width: 40)
+                               // .padding()
+                        }
+                        Text(pseudo)
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                }
+        }
+    }
+}
+
+struct CustList_Previews: PreviewProvider {
+    static var previews: some View {
+        CustList(pseudo: "test")
+    }
+}
---
+++ Cat/ViewModel/UserWaitVM.swift
@@ -0,0 +1,24 @@
+//
+//  UserWaitVM.swift
+//  Cat
+//
+//  Created by Amelie Pocchiolo on 01/12/2022.
+//
+
+import Foundation
+
+    
+var users: [User] = [
+    
+    User(id: 1, mail: "user1@mail.com", pseudo: "Sandra", totPoint: 100, gameCounter: 10),
+    User(id: 2, mail: "user2@mail.com", pseudo: "Lili", totPoint: 20, gameCounter: 10),
+    User(id: 3, mail: "user3@mail.com", pseudo: "Coucou", totPoint: 50, gameCounter: 10),
+    User(id: 4, mail: "user4@mail.com", pseudo: "Sara", totPoint: 125, gameCounter: 10),
+    User(id: 5, mail: "user5@mail.com", pseudo: "Loulou", totPoint: 140, gameCounter: 10),
+    User(id: 6, mail: "user6@mail.com", pseudo: "Minou", totPoint: 90, gameCounter: 10),
+    User(id: 7, mail: "user7@mail.com", pseudo: "Alpha", totPoint: 50, gameCounter: 10),
+    User(id: 8, mail: "user8@mail.com", pseudo: "Beta", totPoint: 125, gameCounter: 10),
+    User(id: 9, mail: "user9@mail.com", pseudo: "Gamma", totPoint: 200, gameCounter: 10),
+    User(id: 10, mail:"user10@mail.com", pseudo: "kikou", totPoint: 90, gameCounter: 10)
+    
+]
